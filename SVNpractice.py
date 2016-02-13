15c0ee759df27e206f3fca9411e5908196a60837
drewski13 <shane.h.andrews@gmail.com>
2016-02-12 23:41:11 -0500

    2nd commit with added functionality(HW)

--- SVNpractice.py
+++ SVNpractice.py
@@ -1,3 +1,9 @@
 #! /usr/bin/python
 
 #my name is Shane Andrews <- wanted to say Tyler Durden haha
+
+#new stuff to say what this is for. i just learned the hard way, emphasis on "hard", how to get a file into an existing SVN repository
+#modify it and get it pushed out to the server i.e. github in this case. the trick was figuring out what "views" were for what
+#and to update and then commit. Synchronize view finally did the trick for me. now for some hello world action
+
+print "Hello World!";
